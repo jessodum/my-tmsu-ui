63703c6cd996f689bc8218746862f2b8bb3b267b
Jess Odum <jessodum@hotmail.com>
2024-03-31 17:09:12 -0600

    add MyTMSUUI_TaggedValue type

---
+++ inc/mytmsuui_taggedvalue.h
@@ -0,0 +1,78 @@
+#ifndef MYTMSUUI_TAGGEDVALUE_H
+#define MYTMSUUI_TAGGEDVALUE_H
+
+#include <QString>
+
+namespace MyTMSUUI_Tagged_NS
+{
+   enum ImpliedState
+   {
+      NormalExplicitTag,
+      ImpliedTag,
+      BothTag
+   };
+}
+
+struct MyTMSUUI_TaggedValue
+{
+   QString myTagName;
+   QString myValue;
+   MyTMSUUI_Tagged_NS::ImpliedState myImpliedState;
+
+   MyTMSUUI_TaggedValue()
+    : myTagName("")
+    , myValue("")
+    , myImpliedState(MyTMSUUI_Tagged_NS::NormalExplicitTag)
+   {
+      //// Does nothing but initialize (yet)
+   }
+
+   // Default d'tor
+   ~MyTMSUUI_TaggedValue()
+   {
+      //// Does nothing (yet)
+   }
+
+   bool operator==(const MyTMSUUI_TaggedValue& other) const;
+   bool operator!=(const MyTMSUUI_TaggedValue& other) const;
+   bool operator==(const QString& otherTagName) const;
+   bool operator!=(const QString& otherTagName) const;
+   friend bool operator== (const QString& otherTagName, const MyTMSUUI_TaggedValue& me);
+   friend bool operator!= (const QString& otherTagName, const MyTMSUUI_TaggedValue& me);
+};
+
+//// =======
+//// INLINES
+//// =======
+
+inline bool MyTMSUUI_TaggedValue::operator==(const MyTMSUUI_TaggedValue& other) const
+{
+   return myTagName == other.myTagName;
+}
+
+inline bool MyTMSUUI_TaggedValue::operator!=(const MyTMSUUI_TaggedValue& other) const
+{
+   return myTagName != other.myTagName;
+}
+
+inline bool MyTMSUUI_TaggedValue::operator==(const QString& otherTagName) const
+{
+   return myTagName == otherTagName;
+}
+
+inline bool MyTMSUUI_TaggedValue::operator!=(const QString& otherTagName) const
+{
+   return myTagName != otherTagName;
+}
+
+inline bool operator== (const QString& otherTagName, const MyTMSUUI_TaggedValue& me)
+{
+   return me == otherTagName;
+}
+
+inline bool operator!= (const QString& otherTagName, const MyTMSUUI_TaggedValue& me)
+{
+   return me != otherTagName;
+}
+
+#endif //// MYTMSUUI_TAGGEDVALUE_H
